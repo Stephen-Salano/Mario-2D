c9609d31a01164d08bc991b08c57c715daab214c
Salano <xirlanostephen@gmail.com>
2026-05-02 00:40:28 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("java")
+}
+
+group = "com"
+version = "1.0-SNAPSHOT"
+val lwjglVersion = "3.4.1"
+val jomlVersion = "1.10.8"
+val lwjglNatives = "natives-linux"
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl:lwjgl")
+    implementation("org.lwjgl:lwjgl-assimp")
+    implementation("org.lwjgl:lwjgl-glfw")
+    implementation("org.lwjgl:lwjgl-nfd")
+    implementation("org.lwjgl:lwjgl-openal")
+    implementation("org.lwjgl:lwjgl-opengl")
+    implementation("org.lwjgl:lwjgl-stb")
+
+    implementation("org.lwjgl:lwjgl::$lwjglNatives")
+    implementation("org.lwjgl:lwjgl-assimp::$lwjglNatives")
+    implementation("org.lwjgl:lwjgl-glfw::$lwjglNatives")
+    implementation("org.lwjgl:lwjgl-nfd::$lwjglNatives")
+    implementation("org.lwjgl:lwjgl-openal::$lwjglNatives")
+    implementation("org.lwjgl:lwjgl-opengl::$lwjglNatives")
+    implementation("org.lwjgl:lwjgl-stb::$lwjglNatives")
+
+    implementation("org.joml:joml:$jomlVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
